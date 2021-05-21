d9fda01ba96d6d6177b16241f4ab94294c34295c
Abraham Serrano <abrahamserrano@MacBook-Pro-de-Abraham-2.local>
2021-05-21 12:23:56 -0500

    add CDN in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const basePath = ON_GITHUB_PAGES ? '/abrahamsm' : ''
-const assetPrefix = ON_GITHUB_PAGES ? '/abrahamsm/' : ''
+//const assetPrefix = ON_GITHUB_PAGES ? '/abrahamsm/' : ''
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPlugins([
   {
     basePath,
-    assetPrefix
+    assetPrefix: isProd
+      ? 'https://cdn.statically.io/gh/AbrahamSM96/https://abrahamsm96.github.io/master/'
+      : ''
   }
 ])
